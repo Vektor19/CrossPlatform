23b8f4f6f6c74548cd5b71f4e16567aaf13eb723
Vektor19 <viktor.komissarov@knu.ua>
2024-09-27 01:15:46 +0300

    updated Program.cs

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lab1;
+
+string inputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\INPUT.txt");
+string outputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\OUTPUT.txt");
+string[] inputValues = Utils.ReadInput(inputPath);
+string[] solutions = new string[inputValues.Length];
+
+for (int i = 0; i < inputValues.Length; i++)
+{
+    if (!Utils.isInputValid(inputValues[i])) solutions[i] = "Bad input";
+    else solutions[i] = TaskSolver.SolveTask(inputValues[i]).ToString();
+}
+Utils.WriteResult(outputPath, solutions);
