eb20cf3e38b5019d33e9410cf0c4274acc6173ad
Vektor19 <viktor.komissarov@knu.ua>
2024-10-04 20:06:25 +0300

    added tests for TaskSolver

---
+++ Lab2.Tests/TestTaskSolver.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab2.Tests
+{
+    public class TestTaskSolver
+    {
+        [Theory]
+        [MemberData(nameof(TestData.SolveTaskData), MemberType = typeof(TestData))]
+        public void SolveTask_ValidInput_ReturnsCorrectCount(int[][] inputArray, int expectedCount)
+        {
+            int result = TaskSolver.SolveTask(inputArray);
+            Assert.Equal(expectedCount, result);
+        }
+    }
+}
