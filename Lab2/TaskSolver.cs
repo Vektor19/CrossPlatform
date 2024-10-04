77f6baebe2bd6b9e29ff881265bb805b01b1b803
Vektor19 <viktor.komissarov@knu.ua>
2024-10-04 18:20:05 +0300

    added TaskSolver class

---
+++ Lab2/TaskSolver.cs
@@ -0,0 +1,32 @@
+﻿namespace Lab2
+{
+    public class TaskSolver
+    {
+        public static int SolveTask(int[][] inputArray)
+        {
+            int n = inputArray.Length;
+            int m = inputArray[0].Length;
+
+            int[,] pathCost = new int[n, m];
+
+            pathCost[0, 0] = inputArray[0][0];
+
+            for (int i = 1; i < n; ++i)
+            {
+                pathCost[i, 0] = inputArray[i][0] + pathCost[i - 1, 0];
+            }
+            for (int i = 1; i < m; ++i)
+            {
+                pathCost[0, i] = inputArray[0][i] + pathCost[0, i - 1];
+            }
+            for (int i = 1; i < n; ++i)
+            {
+                for (int j = 1; j < m; ++j)
+                {
+                    pathCost[i, j] = Math.Min(pathCost[i - 1, j], pathCost[i, j - 1]) + inputArray[i][j];
+                }
+            }
+            return pathCost[n - 1, m - 1];
+        }
+    }
+}
