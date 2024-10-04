636c1416d6176d22d55dadd8940d129c248658c5
Vektor19 <viktor.komissarov@knu.ua>
2024-10-04 18:20:27 +0300

    updated Program.cs

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lab2;
+
+string inputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\INPUT.txt");
+string outputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\OUTPUT.txt");
+
+List<int[][]> inputArrays = Utils.ProcessInput(Utils.ReadInput(inputPath));
+string[] solutions = new string[inputArrays.Count];
+
+for (int i = 0; i < inputArrays.Count; i++)
+{
+    var arr = inputArrays[i];
+    if (arr is null)
+    {
+        solutions[i] = "Bad input";
+    }
+    else
+    {
+        solutions[i] = TaskSolver.SolveTask(arr).ToString();
+    }
+}
+
+Utils.WriteResult(outputPath, solutions);
