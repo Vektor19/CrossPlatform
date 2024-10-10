de24bce62d722fd708228b01073eeee5b7837bbe
Vektor19 <viktor.komissarov@knu.ua>
2024-10-11 02:06:56 +0300

    updated Program.cs

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lab3;
+
+string inputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\INPUT.txt");
+string outputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\OUTPUT.txt");
+
+try
+{
+    var (n, m, k, capital, cities, roads) = Utils.ProcessInput(Utils.ReadInput(inputPath));
+    var solutions = TaskSolver.SolveTask(n, m, k, capital, cities, roads);
+    Utils.WriteResult(outputPath, solutions);
+
+}
+catch (Exception)
+{
+    Utils.WriteResult(outputPath, new string[] { "Bad Input" });
+}
