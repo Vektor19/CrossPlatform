338767591679cac4caaeceb845cc47b94758878e
Vektor19 <viktor.komissarov@knu.ua>
2024-11-14 02:17:34 +0200

    added labs navigation

---
+++ Lab5/Controllers/LabController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab5.Controllers
+{
+    [Authorize]
+    public class LabController : Controller
+    {
+
+        public IActionResult Lab1()
+        {
+            return View();
+        }
+        public IActionResult Lab2()
+        {
+            return View();
+        }
+        public IActionResult Lab3()
+        {
+            return View();
+        }
+    }
+}
