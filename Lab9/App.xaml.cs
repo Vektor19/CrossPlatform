5b9fab366cf22edd3be5f95aa4bb91658994d51d
Vektor19 <viktor.komissarov@knu.ua>
2024-11-24 23:17:36 +0200

    added Lab9 project

---
+++ Lab9/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab9
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
